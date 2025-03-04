2ba344543307d9f125970b3ea543d80969f6743f
Jatin <jatinkhurana350@gmail.com>
2025-03-04 13:17:34 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,21 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'DOCKERHUBCRED', passwordVariable: 'DOCKER_PASS', usernameVariable: 'DOCKER_USER')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+def call(String dockerHubUser, String dockerHubRepo, String backendTag = 'backend', String frontendTag = 'frontend') {
+    script {
+        echo "Pushing to Docker Hub"
+        
+        withCredentials([usernamePassword(credentialsId: 'dockerhubcred', 
+                                          usernameVariable: 'DOCKER_USER', 
+                                          passwordVariable: 'DOCKER_PASS')]) {
+            sh "docker login -u $DOCKER_USER -p $DOCKER_PASS"
+        }
+
+        // Tag the images
+        sh "docker image tag notes-app-backend:latest ${dockerHubUser}/${dockerHubRepo}:${backendTag}"
+        sh "docker image tag notes-app-frontend:latest ${dockerHubUser}/${dockerHubRepo}:${frontendTag}"
+
+        // Push images to Docker Hub
+        sh "docker push ${dockerHubUser}/${dockerHubRepo}:${backendTag}"
+        sh "docker push ${dockerHubUser}/${dockerHubRepo}:${frontendTag}"
+        
+        echo "Docker images pushed successfully."
+    }
 }
