777a9eb600d217e5b874a50947123c9427547420
Jatin <jatinkhurana350@gmail.com>
2025-03-04 13:07:58 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,1 @@
-def call(string url, string branch){
-  echo "Cloning the repository..."
-                git url: "${url}" , branch: "${branch}"
-                echo "Successfully cloned the repository."
-}
+
